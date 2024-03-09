cf1f1c4e6fb5cea641c784d91043ce154c7fd66a
ramen109 <aa.goodspeed@gmail.com>
2024-03-09 01:06:09 -0600

    Created background with following camera

--- src/gameLayer/gameLayer.cpp
+++ src/gameLayer/gameLayer.cpp
@@ -10,12 +10,23 @@
 #include "imfilebrowser.h"
 #include <gl2d/gl2d.h>
 #include <platformTools.h>
+#include <tiledRenderer.h>
 
+struct GameplayData
+{
+	glm::vec2 playerPos = {100,100};
+};
 
+GameplayData data;
 
 gl2d::Renderer2D renderer;
 
+constexpr int BACKGROUNDS = 3;
+
 gl2d::Texture spaceShipTexture;
+gl2d::Texture backgroundTexture[BACKGROUNDS];
+
+TiledRenderer tiledRenderer[BACKGROUNDS];
 
 bool initGame()
 {
@@ -25,6 +36,17 @@ bool initGame()
 
 	spaceShipTexture.loadFromFile(RESOURCES_PATH "spaceShip/ships/green.png", true);
 	
+	backgroundTexture[0].loadFromFile(RESOURCES_PATH "background1.png", true);
+	backgroundTexture[1].loadFromFile(RESOURCES_PATH "background2.png", true);
+	backgroundTexture[2].loadFromFile(RESOURCES_PATH "background3.png", true);
+	
+	for(int i = 0; i < BACKGROUNDS; i++)
+		tiledRenderer[i].texture = backgroundTexture[i];
+
+	tiledRenderer[0].paralaxStrength = 0;
+	tiledRenderer[1].paralaxStrength = 0.5;
+	tiledRenderer[2].paralaxStrength = 0.7;
+
 	return true;
 }
 
@@ -41,21 +63,65 @@ bool gameLogic(float deltaTime)
 	glClear(GL_COLOR_BUFFER_BIT); //clear screen
 
 	renderer.updateWindowMetrics(w, h);
+
+#pragma region movement
+
+	glm::vec2 move = {};
+
+	if(
+		platform::isButtonHeld(platform::Button::W) ||
+		platform::isButtonHeld(platform::Button::Up)
+		)
+	{
+		move.y = -1;
+	}
+	if(
+		platform::isButtonHeld(platform::Button::S) ||
+		platform::isButtonHeld(platform::Button::Down)
+		)
+	{
+		move.y = 1;
+	}
+	if(
+		platform::isButtonHeld(platform::Button::A) ||
+		platform::isButtonHeld(platform::Button::Left)
+		)
+	{
+		move.x = -1;
+	}
+	if(
+		platform::isButtonHeld(platform::Button::D) ||
+		platform::isButtonHeld(platform::Button::Right)
+		)
+	{
+		move.x = 1;
+	}
+
+	if (move.x != 0 || move.y != 0)
+	{
+		move = glm::normalize(move);
+		move *= deltaTime * 200; // 200 pixels per sec
+		data.playerPos += move; 
+	}
+
+#pragma region render background
+
+	for(int i = 0; i < BACKGROUNDS; i++)
+		tiledRenderer[i].render(renderer);
+
 #pragma endregion
+	
+	renderer.currentCamera.follow(data.playerPos, deltaTime * 150, 10, 200, w, h);
 
-
-
-	renderer.renderRectangle({100,100, 100, 100}, Colors_Blue);
-
+	renderer.renderRectangle({data.playerPos.x, data.playerPos.y, 200, 200}, spaceShipTexture);
 
 	renderer.flush();
 
 
 	//ImGui::ShowDemoWindow();
 
-
 	return true;
-#pragma endregion
+
 
 }
 
---
+++ src/gameLayer/tiledRenderer.cpp
@@ -0,0 +1,25 @@
+#include <tiledRenderer.h>
+
+void TiledRenderer::render(gl2d::Renderer2D &renderer)
+    {
+
+        auto viewRect = renderer.getViewRect();
+        //this allows us to see what the player can
+
+        glm::vec2 paralaxDistance = {viewRect.x, viewRect.y};
+        paralaxDistance *= -paralaxStrength;
+
+        for (int y = -1; y <= 1; y++)
+            for(int x = -1; x <= 1; x++){
+
+                int posX = x + int((viewRect.x  - paralaxDistance.x)/ backgroundSize);
+                int posY = y + int((viewRect.y - paralaxDistance.y)/ backgroundSize);
+
+                renderer.renderRectangle(
+                    glm::vec4{posX, posY, 1, 1} * backgroundSize
+                        + glm::vec4(paralaxDistance, 0,0),
+                     texture);
+            }
+
+        renderer.renderRectangle({0, 0, backgroundSize, backgroundSize}, texture);
+    }
