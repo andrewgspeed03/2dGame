02431e6a8ecb7573921fbc7f1f87b18801010d4f
ramen109 <aa.goodspeed@gmail.com>
2024-03-10 00:34:47 -0600

    fixed issue of enemy ai flip flopping forever

--- src/gameLayer/enemy.cpp
+++ src/gameLayer/enemy.cpp
@@ -26,18 +26,18 @@ bool Enemy::update(float deltaTime, glm::vec2 playerPosition){
         else
             shoot = 0;
 
+
     firedTime -= deltaTime;
     if (firedTime < 0)
         firedTime = 0.f;
 
-    if(glm::length(directionToPlayer + viewDirection) <= 0.2){
+    if(glm::length(directionToPlayer + viewDirection) <= 0.2)
         if(rand() % 2)
             newDirection = glm::vec2(directionToPlayer.y, -directionToPlayer.x);
         else
             newDirection = glm::vec2(-directionToPlayer.y, directionToPlayer.x);
-    }
     else
-        newDirection = deltaTime * turnSpeed * directionToPlayer * viewDirection;
+        newDirection = deltaTime * turnSpeed * directionToPlayer + viewDirection;
 
     float length = glm::length(newDirection);
     viewDirection = glm::normalize(newDirection);
